f1f1bff84b0a1a02f28fd7105c93a94f4c02b530
FedericoTorres <insftorres@gmail.com>
2015-06-23 00:40:40 -0300

    Inicio

---
+++ src/HeartTestDrive.java
@@ -0,0 +1,8 @@
+public class HeartTestDrive 
+{
+    public static void main(String[] args) 
+    {
+	HeartModel heartModel = HeartModel.getInstancia();
+        ControllerInterface model = new HeartController(heartModel);
+    }
+}
